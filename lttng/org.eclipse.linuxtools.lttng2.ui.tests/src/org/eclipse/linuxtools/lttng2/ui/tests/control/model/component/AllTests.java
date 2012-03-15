e06335d8e44f8085509eda73d9da0a88d3a4ee65
Bernd Hufmann <bhufmann@gmail.com>
2012-03-15 13:49:35 -0400

    Internalize API for trace control and move control to lttng2

--- lttng/org.eclipse.linuxtools.lttng2.ui.tests/src/org/eclipse/linuxtools/lttng2/ui/tests/control/model/component/AllTests.java
+++ lttng/org.eclipse.linuxtools.lttng2.ui.tests/src/org/eclipse/linuxtools/lttng2/ui/tests/control/model/component/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Ericsson
+ * Copyright (c) 2011 Ericsson
  * 
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -7,32 +7,24 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *   Francois Chouinard - Initial API and implementation
+ *   Bernd Hufmann - Initial API and implementation
  *******************************************************************************/
-
-package org.eclipse.linuxtools.lttng2.ui.tests;
+package org.eclipse.linuxtools.lttng2.ui.tests.control.model.component;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-/**
- * <b><u>AllTests</u></b>
- * <p>
- */
 public class AllTests {
 
     public static Test suite() {
+        
         TestSuite suite = new TestSuite(AllTests.class.getName());
-        // $JUnit-BEGIN$
-
-        // Plug-in
-        suite.addTestSuite(ActivatorTest.class);
-
-        suite.addTest(org.eclipse.linuxtools.lttng2.ui.tests.control.model.impl.AllTests.suite());
-        suite.addTest(org.eclipse.linuxtools.lttng2.ui.tests.control.service.AllTests.suite());
-        suite.addTest(org.eclipse.linuxtools.lttng2.ui.tests.control.model.component.AllTests.suite());
-
-        // $JUnit-END$
-        return suite;
+        //$JUnit-BEGIN$
+        suite.addTestSuite(TraceControlComponentTest.class);
+        suite.addTestSuite(TraceControlTreeModelTest.class);
+        suite.addTestSuite(TraceControlProviderTests.class);
+        suite.addTestSuite(TraceControlPropertiesTest.class);
+        //$JUnit-END$
+        return new ModelImplTestSetup(suite);
     }
 }
